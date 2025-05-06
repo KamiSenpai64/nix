a61cb5c61d4e7d47eca5cd6afae0399644951c55
KamiSenpai64 <miuletdaniel@gmail.com>
2025-05-06 14:20:41 +0300

    first commit

---
+++ configuration.nix
@@ -0,0 +1,181 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running 'nixos-help').
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Enable flakes
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    settings = {
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.networkmanager.enable = true;
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  
+  # Set your time zone.
+  time.timeZone = "America/New_York"; # Change to your timezone
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbVariant = "";
+
+    # Enable the Ly display manager
+    displayManager = {
+      ly = {
+        enable = true;
+      };
+    };
+
+    # Enable i3wm window manager
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3lock
+        i3blocks
+      ];
+    };
+  };
+
+  # Enable sound with pipewire
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # User account
+  users.users.nixos = {  # Change to your username
+    isNormalUser = true;
+    description = "NixOS User";  # Change to your description
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
+    shell = pkgs.zsh;
+  };
+
+  # Enable home-manager
+  programs.home-manager.enable = true;
+
+  # Enable ZSH
+  programs.zsh.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    # Essentials
+    wget
+    curl
+    git
+    vim
+    neovim
+    htop
+    yazi  # File manager
+    kitty  # Terminal
+    firefox
+    
+    # Development tools
+    gnumake
+    gcc
+    lazygit
+    
+    # Gaming
+    prismlauncher  # Minecraft launcher
+    jdk17  # Java for Minecraft
+    
+    # i3wm utilities
+    rofi  # Application launcher
+    feh  # Image viewer and wallpaper setter
+    nitrogen  # Wallpaper manager
+    picom  # Compositor
+    arandr  # Monitor layout
+    lxappearance  # Theme configuration
+    xfce.thunar  # File manager
+    
+    # System utilities
+    unzip
+    zip
+    flameshot  # Screenshot tool
+    pavucontrol  # Audio control
+    brightnessctl  # Brightness control
+    
+    # Fonts
+    font-awesome
+    nerdfonts
+  ];
+
+  # Set default editor to neovim
+  environment.variables.EDITOR = "nvim";
+
+  # Enable fonts
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+    font-awesome
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono" ]; })
+  ];
+
+  # Enable the OpenSSH daemon
+  services.openssh.enable = true;
+
+  # System state version
+  system.stateVersion = "23.11"; # Don't change this!
+}
---
+++ home.nix
@@ -0,0 +1,188 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "nixos"; # Change to your username
+  home.homeDirectory = "/home/nixos"; # Change to your home directory
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # CLI tools
+    bat # Better cat
+    exa # Better ls
+    ripgrep # Better grep
+    fd # Better find
+    btop # Better top
+    neofetch # System info
+    ranger # File manager
+    tmux # Terminal multiplexer
+    jq # JSON processor
+    
+    # Development
+    vscode # Code editor
+    
+    # Communication
+    discord
+  ];
+
+  # Configure Git
+  programs.git = {
+    enable = true;
+    userName = "Your Name"; # Change this
+    userEmail = "your.email@example.com"; # Change this
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase = false;
+    };
+  };
+
+  # Configure Neovim
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    extraConfig = ''
+      set number
+      set relativenumber
+      set tabstop=2
+      set shiftwidth=2
+      set expandtab
+      set smartindent
+      set cursorline
+    '';
+  };
+
+  # Configure Kitty terminal
+  programs.kitty = {
+    enable = true;
+    theme = "Tokyo Night";
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 12;
+    };
+    settings = {
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      update_check_interval = 0;
+      copy_on_select = "clipboard";
+      window_padding_width = 5;
+    };
+  };
+
+  # Configure ZSH with Oh-My-Zsh
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "sudo" "docker" "history" "colored-man-pages" "command-not-found" ];
+      theme = "robbyrussell";
+    };
+    
+    shellAliases = {
+      ll = "exa -l --icons";
+      la = "exa -la --icons";
+      lt = "exa -T --icons";
+      cat = "bat";
+      top = "btop";
+      vim = "nvim";
+      gs = "git status";
+      gc = "git commit";
+      gp = "git push";
+    };
+    
+    initExtra = ''
+      # Additional ZSH configuration here
+      bindkey '^[[A' history-substring-search-up
+      bindkey '^[[B' history-substring-search-down
+    '';
+  };
+
+  # Configure i3wm
+  xsession.windowManager.i3 = {
+    enable = true;
+    config = {
+      modifier = "Mod4"; # Windows/Super key
+      terminal = "kitty";
+      menu = "rofi -show drun";
+      
+      bars = [
+        {
+          position = "bottom";
+          statusCommand = "i3status";
+          fonts = {
+            names = [ "JetBrainsMono Nerd Font" ];
+            size = 10.0;
+          };
+        }
+      ];
+      
+      keybindings = let
+        modifier = config.xsession.windowManager.i3.config.modifier;
+      in {
+        "${modifier}+Return" = "exec kitty";
+        "${modifier}+Shift+q" = "kill";
+        "${modifier}+d" = "exec rofi -show drun";
+        "${modifier}+f" = "fullscreen toggle";
+        "${modifier}+Shift+space" = "floating toggle";
+        "${modifier}+Shift+c" = "reload";
+        "${modifier}+Shift+r" = "restart";
+        "${modifier}+Shift+e" = "exec i3-nagbar -t warning -m 'Do you want to exit i3?' -b 'Yes' 'i3-msg exit'";
+        "${modifier}+l" = "exec i3lock -c 000000";
+        "${modifier}+h" = "focus left";
+        "${modifier}+j" = "focus down";
+        "${modifier}+k" = "focus up";
+        "${modifier}+l" = "focus right";
+        "${modifier}+Shift+h" = "move left";
+        "${modifier}+Shift+j" = "move down";
+        "${modifier}+Shift+k" = "move up";
+        "${modifier}+Shift+l" = "move right";
+        "${modifier}+1" = "workspace number 1";
+        "${modifier}+2" = "workspace number 2";
+        "${modifier}+3" = "workspace number 3";
+        "${modifier}+4" = "workspace number 4";
+        "${modifier}+5" = "workspace number 5";
+        "${modifier}+6" = "workspace number 6";
+        "${modifier}+7" = "workspace number 7";
+        "${modifier}+8" = "workspace number 8";
+        "${modifier}+9" = "workspace number 9";
+        "${modifier}+0" = "workspace number 10";
+        "${modifier}+Shift+1" = "move container to workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9";
+        "${modifier}+Shift+0" = "move container to workspace number 10";
+        "Print" = "exec flameshot gui";
+      };
+      
+      startup = [
+        { command = "nitrogen --restore"; notification = false; }
+        { command = "picom -b"; notification = false; }
+        { command = "nm-applet"; notification = false; }
+      ];
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+}
